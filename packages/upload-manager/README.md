675c9f1d218e592819bf2bf55f12a82c1f14fa28
lukeburpee <lukeburpee@gmail.com>
2018-11-30 15:21:11 -0500

    add upload-manager package to upload-manager-updates branch

---
+++ packages/upload-manager/README.md
@@ -0,0 +1,1 @@
+# feathers-upload-manager
---
+++ packages/upload-manager/src/upload-base-service.ts
@@ -0,0 +1,33 @@
+import { Id } from '@feathersjs/feathers'
+import { ConnectionServiceClass } from '@feathers-service-manager/core-services'
+
+export default function init (options: ServiceOptions) {
+  return new Service(options)
+}
+
+export class Service extends ConnectionServiceClass {
+	constructor (options: ServiceOptions) {
+		super(options)
+	}
+	public getConnectionType (): string {
+		return 'upload'
+	}
+	public getServiceType (): string {
+		return 'base-service'
+	}
+	public healthCheck (): any {
+		return new Promise(resolve => {
+			resolve('nan')
+		})
+	}
+	public getInfo (): any {
+		return new Promise((resolve) => {
+			resolve('nan')
+		})
+	}
+	public close (): any {
+		return new Promise((resolve) => {
+			resolve('nan')
+		})
+	}
+}
