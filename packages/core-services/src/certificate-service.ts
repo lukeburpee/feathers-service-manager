db569fdeeacce027872f2b403f86f56a6f4be7e1
lukeburpee <lukeburpee@gmail.com>
2018-12-02 02:08:31 -0500

    add basic create implementation as placeholder for cert create implementation

--- packages/core-services/src/certificate-service.ts
+++ packages/core-services/src/certificate-service.ts
@@ -10,4 +10,11 @@ export class ServiceClass extends BaseServiceClass {
 	constructor (options: ServiceOptions) {
 		super(options)
 	}
+	public createImplementation (store: any, data: any, params?: Params): any {
+		let id = data[this.id] || this.generateId();
+		let current = _.extend({}, data, { [this.id]: id });
+
+		return Promise.resolve((store[id] = current))
+			.then(_select(params, this.id));
+	}
 }
