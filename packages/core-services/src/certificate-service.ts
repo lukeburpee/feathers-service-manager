fcdc6921222ed9fb126384a4b72e6c97532b04a2
lukeburpee <lukeburpee@gmail.com>
2018-12-02 03:11:05 -0500

    add generateCertificate method and update createImplementation

--- packages/core-services/src/certificate-service.ts
+++ packages/core-services/src/certificate-service.ts
@@ -3,20 +3,51 @@ import { NotFound } from '@feathersjs/errors'
 import { filterQuery, sorter, _ } from '@feathersjs/commons'
 import { _select } from '@feathers-service-manager/utils'
 import { ServiceClass as BaseServiceClass } from './base-service'
+import { generate } from 'selfsigned'
 
 export default function init (options: ServiceOptions) {
   return new ServiceClass(options)
 }
 
 export class ServiceClass extends BaseServiceClass {
+
+	private defaultSettings?: any;
+
 	constructor (options: ServiceOptions) {
 		super(options)
+
+	}
+	private generateCertificate (data: any): any {
+		if (data.attributes) {
+			if (data.settings) {
+				generate(data.attributes, data.settings, (err, pems) => {
+					if (err) {
+						return Promise.reject(err)
+					}
+					return Promise.resolve(pems)
+				})
+			}
+			generate(data.attributes, this.defaultSettings, (err, pems) => {
+				if (err) {
+					return Promise.reject(err)
+				}
+				return Promise.resolve(pems)
+			})
+		}
+		generate(null, this.defaultSettings, (err, pems) => {
+			if (err) {
+				return Promise.reject(err)
+			}
+			return Promise.resolve(pems)
+		})
 	}
 	public createImplementation (store: any, data: any, params?: Params): any {
 		let id = data[this.id] || this.generateId();
-		let current = _.extend({}, data, { [this.id]: id });
-
-		return Promise.resolve((store[id] = current))
-			.then(_select(params, this.id));
+		this.generateCertificate(data)
+			.then((pems) => {
+				const current = _.extend({}, pems, { [this.id]: id });
+				return Promise.resolve((store[id] = current))
+					.then(_select(params, this.id));
+			})
 	}
 }
