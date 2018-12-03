90b90d211b8d14aec959728be5b23d483556bedd
lukeburpee <lukeburpee@gmail.com>
2018-12-03 17:01:20 -0500

    Create file-service.ts

---
+++ packages/core-services/src/file-service.ts
@@ -0,0 +1,13 @@
+import { Id } from '@feathersjs/feathers'
+import { NotFound } from '@feathersjs/errors'
+import { ServiceClass as BaseServiceClass } from './base-service'
+
+export default function init (options: ServiceOptions) {
+  return new ServiceClass(options)
+}
+
+export class ServiceClass extends BaseServiceClass {
+	constructor (options: ServiceOptions) {
+		super(options)
+	}
+}
