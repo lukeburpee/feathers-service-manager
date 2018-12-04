4d6a1e378e0dfabbd2049f3fd19c17daf0ccaa96
lukeburpee <lukeburpee@gmail.com>
2018-12-03 19:56:48 -0500

    Update certificate-service.test.ts

--- packages/core-services/test/certificate-service.test.ts
+++ packages/core-services/test/certificate-service.test.ts
@@ -11,70 +11,61 @@ import CertificateService, { ServiceClass } from '../src/certificate-service'
 const debug = require('debug')('feathers-service-manager:certificate-service:test')
 
 describe('feathers-service-manager:certificate-service', () => {
-	const app = feathers()
-
 	const options = {
 		events: ['testing']
 	}
-
-	const withDefaults = {
-		events: ['testing']
-	}
-
-	app.use('certificates', CertificateService(options))
-	const service = app.service('certificates')
-
+	//app.use('certificates', CertificateService(options))
+	//const service = app.service('certificates')
 	describe('Requiring', () => {
 		it('exposes the Service Constructor', () => {
 			expect(typeof CertificateService).to.equal('function')
 		})
 	})
-
-	describe('Custom Methods', () => {
-		const rawService = new ServiceClass(options)
-		const rawWithDefaults = new ServiceClass(withDefaults)
-		rawService.setup(app, 'raw-service')
-		rawWithDefaults.setup(app, 'raw-with-defaults')
-		describe('generateCertificate', () => {
-			it('generates pem from provided attributes and settings', () => {
-				return rawService.generateCertificate({
-					attributes: { name: 'test' },
-					settings: { days: 364 }
-				}).then((result: any) => {
-					expect(result).to.have.property('private')
-					expect(result).to.have.property('public')
-					expect(result).to.have.property('cert')
-				})
-			})
-			it('generates pem using default service settings if settings not provided', () => {
-				return rawService.generateCertificate({
-					attributes: { name: 'test' }
-				}).then((result: any) => {
-					expect(result).to.have.property('private')
-					expect(result).to.have.property('public')
-					expect(result).to.have.property('cert')
-				})
-			})
-			it('generates pem from null attributes if attributes not provided', () => {
-				return rawService.generateCertificate({
-				}).then((result: any) => {
-					expect(result).to.have.property('private')
-					expect(result).to.have.property('public')
-					expect(result).to.have.property('cert')
-				})
-			})
-			describe('Invalid Pem Certificate Attributes', () => {
-				it ('throws an error', () => {
-					return rawService.generateCertificate({
-						attributes: { test: 'test' }
-					})
-					.catch((error: any) => {
-						expect(error.message).to.equal('certificate-service error: Invalid certificate attribute test')
-					})
-				})
-			})
-		})
-	})
+	//describe('Custom Methods', () => {
+	//	const rawService = new ServiceClass(options)
+	//	const rawWithDefaults = new ServiceClass(withDefaults)
+	//	rawService.setup(app, 'raw-service')
+	//	rawWithDefaults.setup(app, 'raw-with-defaults')
+	//	describe('generateCertificate', () => {
+	//		it('generates pem from provided attributes and settings', () => {
+	//			return rawService.generateCertificate({
+	//				attributes: { name: 'test' },
+	//				settings: { days: 364 }
+	//			}).then((result: any) => {
+	//				expect(result).to.have.property('private')
+	//				expect(result).to.have.property('public')
+	//				expect(result).to.have.property('cert')
+	//			})
+	//		})
+	//		it('generates pem using default service settings if settings not provided', () => {
+	//			return rawService.generateCertificate({
+	//				attributes: { name: 'test' }
+	//			}).then((result: any) => {
+	//				expect(result).to.have.property('private')
+	//				expect(result).to.have.property('public')
+	//				expect(result).to.have.property('cert')
+	//			})
+	//		})
+	//		it('generates pem from null attributes if attributes not provided', () => {
+	//			return rawService.generateCertificate({
+	//			}).then((result: any) => {
+	//				expect(result).to.have.property('private')
+	//				expect(result).to.have.property('public')
+	//				expect(result).to.have.property('cert')
+	//			})
+	//		})
+	//		describe('Invalid Pem Certificate Attributes', () => {
+	//			it ('throws an error', () => {
+	//				return rawService.generateCertificate({
+	//					attributes: { test: 'test' }
+	//				})
+	//				.catch((error: any) => {
+	//					expect(error.message).to.equal('certificate-service error: Invalid certificate attribute test')
+	//				})
+	//			})
+	//		})
+	//	})
+	//})
 	//describe('Base Service Methods', () => {
 	//	let store = {}
 	//	const rawService = new ServiceClass(options)
@@ -95,11 +86,10 @@ describe('feathers-service-manager:certificate-service', () => {
 	//	})
 	//})
 	describe('Standard Service Methods', () => {
-		const commonApp = feathers()
-		commonApp.use('certificates', CertificateService(options))
-		const certificates = commonApp.service('certificates')
-
 		describe('create', () => {
+			const commonApp = feathers()
+			commonApp.use('certificates', CertificateService(options))
+			const certificates = commonApp.service('certificates')
 			it(`creates and returns pem certificate with provided attributes and settings`, () => {
 				return certificates.create({
 					attributes: {
@@ -155,6 +145,30 @@ describe('feathers-service-manager:certificate-service', () => {
 					}).catch((error: any) => {
 						expect(error.message).to.equal('certificate-service error: Invalid certificate attribute test')
 					})
+				})
+			})
+		})
+		describe('get', () => {
+			const commonApp = feathers()
+			commonApp.use('certificates', CertificateService(options))
+			const certificates = commonApp.service('certificates')
+			const getId = uuid()
+			before((done: any) => {
+				certificates.create({
+					id: getId,
+					attributes: {
+						name: 'test'
+					}
+				}).then(() => {
+					done()
+				})
+			})
+			it('returns a certificate by id', () => {
+				return certificates.get(getId).then((result: any) => {
+					expect(result).to.have.property('id', getId)
+					expect(result).to.have.property('private')
+					expect(result).to.have.property('public')
+					expect(result).to.have.property('cert')
 				})
 			})
 		})
