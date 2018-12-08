5902956a9b36a94ed1d1a1ff58f0683536d1ed80
lukeburpee <lukeburpee@gmail.com>
2018-12-07 19:05:30 -0500

    create pouch and orbit sync initial commits

---
+++ packages/pouch-sync/test/orbit-services.test.ts
@@ -0,0 +1,58 @@
+import { expect } from 'chai';
+import feathers from '@feathersjs/feathers';
+import { v4 as uuid } from 'uuid'
+import { default as Debug } from 'debug'
+import { ServiceClass } from '../src/pouch-base-service'
+
+const debug = Debug('@feathers-service-manager:pouch-sync:test')
+
+describe('@feathers-service-manager:pouch-sync', () => {
+	const app = feathers()
+	const serviceOptions = {
+		connectionId: uuid(),
+		client: 'client',
+		events: ['testing']
+	}
+	describe('Base Service', () => {
+		const rawService = new ServiceClass(serviceOptions)
+		rawService.setup(app, '/pouch-service')
+		describe('Connection Methods', () => {
+			describe('getConnectionType', () => {
+				it(`returns the 'pouch' connection type`, () => {
+					expect(rawService.getConnectionType()).to.equal('pouch')
+				})
+			})
+			describe('getServiceType', () => {
+				it(`returns the 'base-service' pouch service type`, () => {
+					expect(rawService.getServiceType()).to.equal('base-service')
+				})
+			})
+			describe('healthCheck', () => {
+				it(`returns the results of the pouch server health check`, () => {
+					return rawService.healthCheck().then((status: any) => {
+						expect(status).to.equal('nan')
+					})
+				})
+			})
+			describe('getInfo', () => {
+				it(`returns the results of the pouch info check`, () => {
+					return rawService.getInfo().then((info: any) => {
+						expect(info).to.equal('nan')
+					})
+				})
+			})
+			describe('close', () => {
+				it(`closes the pouch client connection`, () => {
+					return rawService.close().then((results: any) => {
+						expect(results).to.equal('nan')
+					})
+				})
+			})
+		})
+		// app.use('d-service', DockerService(serviceOptions))
+		// const dService = app.service('d-service')
+		// describe('Common Service Tests', () => {
+		// base(app, errors, 'd-service', 'id')
+		// })
+	})
+})
