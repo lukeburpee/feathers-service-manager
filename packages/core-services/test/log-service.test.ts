0841c34c3ed20c72620b1e96db0e169166a2f07f
lukeburpee <lukeburpee@gmail.com>
2018-12-10 19:42:47 -0500

    add initial commit for log-service tests

---
+++ packages/core-services/test/log-service.test.ts
@@ -0,0 +1,18 @@
+import feathers from '@feathersjs/feathers';
+import { default as Debug } from 'debug'
+import LogService from '../src/log-service'
+
+const debug = Debug('feathers-service-manager:log-service:test')
+
+describe('feathers-service-manager:log-service', () => {
+	debug('log-service tests starting')
+	const options = {
+		events: ['testing']
+	}
+
+	const app = feathers()
+	app.use('logs', LogService(options))
+	// describe('Common Service Tests', () => {
+	// base(app, errors, 'json')
+	// })
+})
