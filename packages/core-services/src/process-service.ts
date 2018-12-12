61e15a4c59af4ca5d37d3eb2e566b06477476c17
lukeburpee <lukeburpee@gmail.com>
2018-12-12 17:14:33 -0500

    update process-service
    - execute and kill methods
    - basic initialization test
    - add process-service input to service options type

--- packages/core-services/src/process-service.ts
+++ packages/core-services/src/process-service.ts
@@ -1,6 +1,7 @@
 import { default as Debug } from 'debug'
+import execa from 'execa'
 
-import { ServiceClass as BaseServiceClass } from './base-service'
+import { ServiceClass as MultiServiceClass } from './multi-service'
 
 const debug = Debug('feathers-service-manager:core-services:process-service')
 
@@ -8,9 +9,40 @@ export default function init (options: ServiceOptions) {
   return new ServiceClass(options)
 }
 
-export class ServiceClass extends BaseServiceClass {
+export class ServiceClass extends MultiServiceClass {
+	public processId!: any;
+	public processes!: any;
 	constructor(options: ServiceOptions) {
 		super(options)
+		this.processId = options.processId || 'processId'
 	}
-	
+	public async setup (app: any, path: any): Promise<any> {
+		super.setup(app, path)
+		await this.addService({
+			app,
+			service: 'processes',
+			serviceOptions: {
+				id: this.processId
+			}
+		}).then((processes: any) => {
+			this.processes = processes.service
+		})
+	}
+
+	public async execute (data: any): Promise<any> {
+		if (!data.command) {
+			throw new Error('execute process requires a command.')
+		}
+		let p = execa(data.command, data.args || [], data.options || [])
+		return Promise.resolve(await this.processes.create({
+			[this.processId]: data.id || p.pid,
+			...p
+		}))
+	}
+
+	public async kill (id: any): Promise<any> {
+		let { pid } = await this.processes.get(id)
+		process.kill(pid, 'SIGINT')
+		return Promise.resolve(await this.processes.remove(id))
+	}
 }
--- packages/core-services/test/process-service.test.ts
+++ packages/core-services/test/process-service.test.ts
@@ -1,17 +1,26 @@
+import { expect } from 'chai'
 import feathers from '@feathersjs/feathers';
 import { default as Debug } from 'debug'
-import ProcessService from '../src/process-service'
+import ProcessService, { ServiceClass } from '../src/process-service'
 
 const debug = Debug('feathers-service-manager:process-service:test')
 
 describe('feathers-service-manager:process-service', () => {
 	debug('process-service tests starting')
+	const app = feathers()
+
 	const options = {
 		events: ['testing']
 	}
 
-	const app = feathers()
-	app.use('processes', ProcessService(options))
+	describe('Initialization', () => {
+		const rawService = new ServiceClass(options)
+		rawService.setup(app, '/test')
+		it('adds processes service to app', () => {
+			expect(app.service('processes')).to.not.equal(undefined)
+			expect(rawService.processes._id).to.equal('processId')
+		})
+	})
 	// describe('Common Service Tests', () => {
 	// base(app, errors, 'json')
 	// })
