bc36c70f273720b6a35a12c2dbb1cc32da37fe20
lukeburpee <lukeburpee@gmail.com>
2018-12-13 21:00:45 -0500

    update json service

--- packages/core-services/src/json-service.ts
+++ packages/core-services/src/json-service.ts
@@ -1,4 +1,8 @@
+import load from 'load-json-file'
+import write from 'write-json-file'
+
 import { ServiceClass as BaseServiceClass } from './base-service'
+
 import { default as Debug } from 'debug'
 
 const debug = Debug('feathers-service-manager:core-services:json-service')
@@ -8,8 +12,18 @@ export default function init (options: ServiceOptions) {
 }
 
 export class ServiceClass extends BaseServiceClass {
+	public json!: any;
 	constructor (options: ServiceOptions) {
 		super(options)
-		debug('json-service initialized')
+		if (!options.file) {
+			throw new Error(`json service requires a file path.`)
+		}
+	}
+	public setup (app: any, path: any): any {
+		return load(this.options.filePath)
+			.then((result: any) => {
+				this.json = result
+				debug('json-service initialized')
+			})
 	}
 }
