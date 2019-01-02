58fd047f84f88c6972fbc6817d943df595a7e313
lukeburpee <lukeburpee@gmail.com>
2019-01-01 19:47:46 -0500

    fix coverage and task/job services

---
+++ packages/core-services/src/task-service.ts
@@ -0,0 +1,16 @@
+import { ServiceClass as BaseServiceClass } from './base-service'
+
+import { default as Debug } from 'debug'
+
+const debug = Debug('feathers-service-manager:core-services:task-service')
+
+export default function init (options: ServiceOptions) {
+	return new ServiceClass(options)
+}
+
+export class ServiceClass extends BaseServiceClass {
+	constructor (options: ServiceOptions) {
+		super(options)
+		debug('task-service initialized')
+	}
+}
