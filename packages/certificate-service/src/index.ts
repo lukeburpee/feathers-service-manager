17970b3676c5d814d291457110c99e72f728070d
lukeburpee <lukeburpee@gmail.com>
2019-01-02 19:20:20 -0500

    update certificate service createImplementation and generateCertificate methods

--- packages/certificate-service/src/index.ts
+++ packages/certificate-service/src/index.ts
@@ -49,9 +49,10 @@ export class ServiceClass extends BaseServiceClass {
 	}
 
 	public async generateCertificate (data?: any): Promise<any> {
+		let attributes = this.validateCertAttributes(data.attributes)
+		let settings = data.settings || this.defaultSettings
 		return new Promise((resolve, reject) => {
-			return generate (
-				this.validateCertAttributes(data.attributes), data.settings || this.defaultSettings, (err: any, pems: any) => {
+			return generate (attributes, settings, (err: any, pems: any) => {
 					if (err) {
 						return reject(err)
 					}
@@ -67,15 +68,15 @@ export class ServiceClass extends BaseServiceClass {
 
 	public async createImplementation (store: any, data: any, params?: Params): Promise<any> {
 		let id = data[this.id] || this.generateId();
-		return this.generateCertificate(data)
-			.then((pems: any) => {
-				const current = _.extend({},
-					{ attributes: data.attributes || null },
-					{ settings: data.settings || null },
-					pems, { [this.id]: id });
-				return Promise.resolve((store[id] = current))
-					.then(_select(params, this.id));
-			})
+		let pems = await this.generateCertificate(data)
+		const current = _.extend({},
+			{ [this.id]: id },
+			{ attributes: data.attributes || null },
+			{ settings: data.settings || null },
+			pems
+		)
+		return Promise.resolve((store[id] = current))
+			.then(_select(params, this.id))
 	}
 
 	public updateImplementation (store: any, id: Id, data: any, params?: Params): any {
